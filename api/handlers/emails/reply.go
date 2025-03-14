0842a660966981344aabe9f103171f836e929b81
mattbr0wn <matt@openline.ai>
2025-03-14 17:29:35 +0000

    reply to email

--- api/handlers/emails/reply.go
+++ api/handlers/emails/reply.go
@@ -1,9 +1,9 @@
 package emails
 
 import (
+	"context"
 	"fmt"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -15,199 +15,178 @@ import (
 	"github.com/customeros/mailstack/internal/models"
 	"github.com/customeros/mailstack/internal/tracing"
 	"github.com/customeros/mailstack/internal/utils"
-	"github.com/customeros/mailstack/services/smtp"
 )
 
 type ReplyEmailRequest struct {
-	FromAddress  string        `json:"fromAddress"`
-	ToAddresses  []string      `json:"toAddresses"`
-	CCAddresses  []string      `json:"ccAddresses"`
-	BCCAddresses []string      `json:"bccAddresses"`
-	ReplyTo      string        `json:"replyTo"`
-	Subject      string        `json:"subject"`
-	Body         EmailBody     `json:"body"`
-	Attachments  []Attachments `json:"attachments"`
-	ScheduleFor  time.Time     `json:"scheduleFor"`
+	ReplyTo     string        `json:"replyTo"`
+	Body        EmailBody     `json:"body"`
+	Attachments []Attachments `json:"attachments"`
+	ScheduleFor time.Time     `json:"scheduleFor"`
 }
 
 // reply to an existing email thread
 func (h *EmailsHandler) Reply() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		ctx, span := tracing.StartHttpServerTracerSpanWithHeader(c.Request.Context(), "EmailsHandler.Send", c.Request.Header)
+		ctx, span := tracing.StartHttpServerTracerSpanWithHeader(c.Request.Context(), "EmailsHandler.Reply", c.Request.Header)
 		defer span.Finish()
 		tracing.TagComponentRest(span)
 		tracing.TagTenant(span, utils.GetTenantFromContext(ctx))
 
-		emailContainer, err := h.validateSendEmailRequest(c)
-		if err != nil {
-			tracing.TraceErr(span, err)
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		// Parse the request
+		var request ReplyEmailRequest
+		if err := c.ShouldBindJSON(&request); err != nil {
+			h.respondWithError(c, span, http.StatusBadRequest, "Invalid request format", err)
 			return
 		}
 
-		// create new email thread
-		threadId, err := h.repositories.EmailThreadRepository.Create(ctx, &models.EmailThread{
-			MailboxID:      emailContainer.Mailbox.ID,
-			Subject:        emailContainer.Email.Subject,
-			Participants:   emailContainer.Email.AllParticipants(),
-			MessageCount:   1,
-			LastMessageID:  emailContainer.Email.MessageID,
-			HasAttachments: emailContainer.Email.HasAttachment,
-			FirstMessageAt: utils.NowPtr(),
-			LastMessageAt:  utils.NowPtr(),
-		})
+		// validate email exists
+		replyToEmailID := c.Param("id")
+		if replyToEmailID == "" {
+			h.respondWithError(c, span, http.StatusBadRequest, "emailID cannot be empty", nil)
+			return
+		}
+		replyToEmail, err := h.repositories.EmailRepository.GetByID(ctx, replyToEmailID)
 		if err != nil {
-			tracing.TraceErr(span, err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			h.respondWithError(c, span, http.StatusInternalServerError, "error retrieving email", err)
+			return
+		}
+		if replyToEmail == nil {
+			h.respondWithError(c, span, http.StatusNotFound, fmt.Sprintf("cannot find email with id %s", replyToEmailID), nil)
 			return
 		}
 
-		// save email record
-		emailContainer.Email.ThreadID = threadId
-		emailContainer.Email.Status = enum.EmailStatusQueued
-		emailContainer.Email.SendAttempts = 1
-		id, err := h.repositories.EmailRepository.Create(ctx, emailContainer.Email)
+		// Validate request
+		errs := h.ValidateReplyRequest(ctx, &request)
+		if errs.HasErrors() {
+			tracing.TraceErr(span, errs)
+			c.JSON(http.StatusBadRequest, errs)
+		}
+
+		// Build email container
+		emailContainer, err := h.buildReplyEmailContainer(ctx, &request, replyToEmail)
 		if err != nil {
-			tracing.TraceErr(span, err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			h.respondWithError(c, span, http.StatusInternalServerError, "error retrieving email", err)
 			return
 		}
-		emailContainer.Email.ID = id
+		if emailContainer == nil {
+			h.respondWithError(c, span, http.StatusNotFound, fmt.Sprintf("cannot find email thread with id %s", replyToEmailID), nil)
+			return
+		}
 
-		smtpClient := smtp.NewSMTPClient(h.repositories, emailContainer.Mailbox)
-
-		results := smtpClient.Send(ctx, emailContainer.Email, emailContainer.Attachments)
-		if !results.Success {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": results.ErrorMessage,
-			})
+		// Send reply
+		result, err := h.sendEmail(ctx, emailContainer)
+		if err != nil {
+			h.respondWithError(c, span, http.StatusInternalServerError, "Failed to send email", err)
 			return
 		}
 
 		c.JSON(http.StatusOK, gin.H{
-			"messageId": results.MessageID,
+			"messageId": result.MessageID,
 		})
 	}
 }
 
-func (h *EmailsHandler) validateReplyEmailRequest(c *gin.Context) (EmailContainer, error) {
-	span, ctx := opentracing.StartSpanFromContext(c.Request.Context(), "EmailsHandler.validateSendEmailRequest")
+func (h *EmailsHandler) ValidateReplyRequest(ctx context.Context, request *ReplyEmailRequest) *custom_err.MultiErrors {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "EmailsHandler.validateRequest")
 	defer span.Finish()
 	tracing.TagComponentRest(span)
 
-	errs := custom_err.NewMultiErrors()
-	emailContainer := EmailContainer{}
-
-	var emailData SendEmailRequest
-	err := c.ShouldBindJSON(&emailData)
-	if err != nil {
-		tracing.TraceErr(span, err)
-		errs.Add("request", "please provide a valid request payload", errors.New("cannot parse request"))
-		return emailContainer, errs
-	}
-
-	if emailData.MailboxID == "" && emailData.FromAddress == "" {
-		errs.Add("sender", "provide either a valid mailboxId or fromAddress", errors.New("Invalid sender"))
-		tracing.TraceErr(span, err)
-		return emailContainer, err
-	}
-
-	if emailData.MailboxID != "" {
-		mailbox, err := h.repositories.MailboxRepository.GetMailbox(ctx, emailData.MailboxID)
-		if err != nil || mailbox == nil {
-			errs.Add("mailboxId", "unable to identify mailbox", err)
-			tracing.TraceErr(span, err)
-		}
-		emailContainer.Mailbox = mailbox
-	}
-
-	if emailContainer.Mailbox == nil {
-		err := h.validateEmailSyntax(ctx, &emailData.FromAddress)
-		if err != nil {
-			errs.Add("fromAddress", fmt.Sprintf("%s is invalid", emailData.FromAddress), err)
-			tracing.TraceErr(span, err)
-		}
-		mailbox, err := h.repositories.MailboxRepository.GetMailboxByEmailAddress(ctx, emailData.FromAddress)
-		if err != nil || mailbox == nil {
-			errs.Add("mailboxId", "unable to identify mailbox", err)
-			tracing.TraceErr(span, err)
-		}
-		emailContainer.Mailbox = mailbox
-	}
-
-	if !emailContainer.Mailbox.OutboundEnabled {
-		err := errors.New("mailbox not enabled for outbound")
-		tracing.TraceErr(span, err)
-		errs.Add("mailbox", "please enable mailbox for sending", err)
-	}
-
-	if len(emailData.ToAddresses) == 0 {
-		err := errors.New("toAddresses is empty")
-		errs.Add("toAddresses", "please provide at least one valid to address", err)
-		tracing.TraceErr(span, err)
-	}
-
-	emailData.ToAddresses = h.validateEmailAddresses(ctx, "toAddresses", emailData.ToAddresses, errs)
-	emailData.CCAddresses = h.validateEmailAddresses(ctx, "ccAddresses", emailData.CCAddresses, errs)
-	emailData.BCCAddresses = h.validateEmailAddresses(ctx, "bccAddresses", emailData.BCCAddresses, errs)
-
-	if emailData.ReplyTo != "" {
-		err := h.validateEmailSyntax(ctx, &emailData.ReplyTo)
-		if err != nil {
-			errs.Add("replyTo", fmt.Sprintf("%s is invalid", emailData.ReplyTo), err)
-			tracing.TraceErr(span, err)
-		}
-	}
-
-	if emailData.Subject == "" {
-		errs.Add("subject", "please provide an email subject", errors.New("subject is empty"))
-	}
-
-	if emailData.Body.HTML == "" && emailData.Body.Text == "" {
+	var errs custom_err.MultiErrors
+	if request.Body.HTML == "" && request.Body.Text == "" {
 		errs.Add("body", "please provide a valid html or text body (or both)", errors.New("body is empty"))
 	}
+	return &errs
+}
 
-	if len(emailData.Attachments) > 0 {
-		emailContainer.Attachments = h.validateAttachments(ctx, emailData.Attachments, errs)
+func (h *EmailsHandler) buildReplyEmailContainer(ctx context.Context, request *ReplyEmailRequest, replyToEmail *models.Email) (*EmailContainer, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "EmailsHandler.buildReplyEmailContainer")
+	defer span.Finish()
+	tracing.TagComponentRest(span)
+
+	// Create container
+	container := &EmailContainer{}
+
+	// Get mailbox
+	mailbox, err := h.repositories.MailboxRepository.GetMailbox(ctx, replyToEmail.MailboxID)
+	if err != nil {
+		tracing.TraceErr(span, err)
+		return nil, err
+	}
+	container.Mailbox = mailbox
+
+	// Validate outbound capability
+	if !mailbox.OutboundEnabled {
+		tracing.TraceErr(span, err)
+		return nil, errors.New("mailbox not enabled for outbound email")
 	}
 
-	if errs.HasErrors() {
-		return emailContainer, errs
+	// Validate reply-to
+	replyTo := request.ReplyTo
+	if replyTo != "" {
+		cleanReplyTo, err := h.validateEmailSyntax(ctx, replyTo)
+		if err != nil {
+			tracing.TraceErr(span, err)
+			return nil, fmt.Errorf("invalid replyTo address: %w", err)
+		}
+		replyTo = cleanReplyTo
 	}
 
-	// principle: data passed in api request takes priority over default vales stored on mailbox.
-	// default values stored on mailbox will be used where data is not provided in api
-	emailRecord := models.Email{
-		MailboxID:    emailContainer.Mailbox.ID,
+	// Validate attachments
+	attachments, err := h.resolveAttachments(ctx, request.Attachments)
+	if err != nil {
+		tracing.TraceErr(span, err)
+		return nil, err
+	}
+	container.Attachments = attachments
+
+	// build email model
+	email := &models.Email{
+		MailboxID:    mailbox.ID,
 		Direction:    enum.EmailOutbound,
-		MessageID:    utils.GenerateMessageID(emailContainer.Mailbox.MailboxDomain, ""),
-		Subject:      emailData.Subject,
-		FromDomain:   emailContainer.Mailbox.MailboxDomain,
-		FromAddress:  emailData.FromAddress,
-		FromUser:     strings.Split(emailData.FromAddress, "@")[0],
-		FromName:     emailData.FromName,
-		ToAddresses:  emailData.ToAddresses,
-		CcAddresses:  emailData.CCAddresses,
-		BccAddresses: emailData.BCCAddresses,
-		ReplyTo:      emailData.ReplyTo,
-		BodyText:     emailData.Body.Text,
-		BodyHTML:     emailData.Body.HTML,
+		ThreadID:     replyToEmail.ThreadID,
+		InReplyTo:    replyToEmail.MessageID,
+		Subject:      fmt.Sprintf("RE: %s", replyToEmail.CleanSubject),
+		FromAddress:  mailbox.EmailAddress,
+		FromName:     mailbox.DefaultFromName,
+		FromUser:     mailbox.MailboxUser,
+		FromDomain:   mailbox.MailboxDomain,
+		ReplyTo:      replyTo,
+		ToAddresses:  []string{replyToEmail.FromAddress},
+		BodyText:     request.Body.Text,
+		BodyHTML:     request.Body.HTML,
+		Status:       enum.EmailStatusQueued,
+		SendAttempts: 1,
 	}
 
-	if emailRecord.FromAddress == "" {
-		emailRecord.FromAddress = emailContainer.Mailbox.EmailAddress
-	}
-	if emailRecord.FromName == "" {
-		emailRecord.FromName = emailContainer.Mailbox.DefaultFromName
-	}
-	if emailRecord.ReplyTo == "" && emailContainer.Mailbox.ReplyToAddress == "" {
-		emailRecord.ReplyTo = emailContainer.Mailbox.ReplyToAddress
-	}
-	if len(emailContainer.Attachments) > 0 {
-		emailRecord.HasAttachment = true
+	// if reply-to header set on replyToEmail, use reply-to address as to
+	if replyToEmail.ReplyTo != "" {
+		replyTo := []string{replyToEmail.ReplyTo}
+		email.ToAddresses = replyTo
 	}
 
-	emailContainer.Email = &emailRecord
+	if len(request.Attachments) > 0 {
+		email.HasAttachment = true
+	}
 
-	return emailContainer, nil
+	// build references from all previous emails in thread
+	threadRecord, err := h.repositories.EmailRepository.ListByThread(ctx, replyToEmail.ThreadID)
+	if err != nil {
+		tracing.TraceErr(span, err)
+		return nil, err
+	}
+	if threadRecord == nil {
+		err := errors.New("unable to identify email thread")
+		tracing.TraceErr(span, err)
+		return nil, err
+	}
+
+	for _, e := range threadRecord {
+		messageID := fmt.Sprintf("<%s>", e.MessageID)
+		if !utils.IsStringInSlice(messageID, email.References) {
+			email.References = append(email.References, messageID)
+		}
+	}
+
+	container.Email = email
+	return container, nil
 }
