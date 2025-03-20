bffa159f66cfcbbb37bc39c1ae8a317389fd311a
mattbr0wn <matt@openline.ai>
2025-03-20 15:50:36 +0000

    patch: rename endpoints

--- api/graphql/resolver/threads_resolvers.go
+++ api/graphql/resolver/threads_resolvers.go
@@ -11,10 +11,22 @@ import (
 	"github.com/customeros/mailstack/api/graphql/graphql_model"
 )
 
-// GetThreadsByUser is the resolver for the getThreadsByUser field.
-func (r *queryResolver) GetThreadsByUser(ctx context.Context, userID string) ([]*graphql_model.EmailThread, error) {
+// GetAllThreads is the resolver for the getAllThreads field.
+func (r *queryResolver) GetAllThreads(ctx context.Context, userID string) ([]*graphql_model.EmailThread, error) {
+	panic(fmt.Errorf("not implemented: GetAllThreads - getAllThreads"))
+}
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+/*
+	func (r *queryResolver) GetThreadsByUser(ctx context.Context, userID string) ([]*graphql_model.EmailThread, error) {
 	// get all mailboxes for user
 
 	// get all threads for each mailbox
 	panic(fmt.Errorf("not implemented: GetThreadsByUser - getThreadsByUser"))
 }
+*/
